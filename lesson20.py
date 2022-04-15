3cc0754b5fe04628d0e727e4549f300a43aa50a0
AnushavanNavasardyanAnushavan <navasardyan.anushavan@gmail.com>
2022-04-15 19:38:10 +0400

    lesson20

---
+++ lesson20.py
@@ -0,0 +1,13 @@
+import module as m
+
+# N1
+# print(m.facr(6))
+
+# N2
+# print(m.cylinder(5,4))
+
+# N3
+# print(m.sphere(5))
+
+# N4
+# print(m.convert(90))
---
+++ module.py
@@ -0,0 +1,134 @@
+# N1
+# def facr(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * facr(n - 1)
+
+# print(facr(5))
+
+# N2
+# import math  as m 
+
+# def cylinder(h, r):
+#     v = m.pi * (r ** 2)
+#     r = (2 * m.pi *r * h) + (2 * m.pi * (r ** 2))
+#     return "V = ", v, "R = ", r
+# # print(cylinder( 5,  4))
+
+
+
+# N3
+# import math  as m 
+
+# def sphere(r):
+#     v =  4 / 3 * m.pi * (r ** 3)
+#     a = 4 * m.pi * (r ** 2)
+#     return "V = ", v, "A = ", a
+# # print(sphere(5))
+
+
+# N4
+# import math  as m 
+
+# def  convert(rad):
+#     r = (rad * m.pi) / 180 
+#     return r
+# # print(convert(90))
+
+
+# N5
+# def specifiedNum
+# ber(n):
+#     for i in range(2, n+1):
+#         for j in range(2, i):
+#             if i % j == 0:
+#                 break
+#         else:
+#             print(i)
+# specifiedNumber(10)
+
+
+
+# //////lesson7
+# N1
+# def phone(color, phone):
+#     c = ["red", 'blue']
+#     m = ["iphone 12", 'samsung']
+#     for i, j in c, m:
+#         if color in c and phone in m:
+#             print("true")
+#             break
+#     else:
+#         print("false")
+
+# phone("red", 'iphone ')
+
+
+
+
+# N2
+# def questions():
+#     while True:
+#         questions1 = input("enter answers 1 a, b, c, d,----> ") == "a" 
+#         questions2 = input("enter answers 2 a, b, c, d,----> ") == "b"
+#         questions3 = input("enter answers 3 a, b, c, d,----> ") == "c"
+#         questions4 = input("enter answers 4 a, b, c, d,----> ") == "d"
+#         questions5 = input("enter answers 5 a, b, c, d,----> ") == "a"
+#         if questions1 == True and questions2 == True and questions3 == True and questions4 == True and questions4 == True and questions5 == True:
+#             print("user win")
+#             break     
+#         else:
+#             print("user loze") 
+#             break 
+# questions()
+
+
+
+
+# lesson9
+# N2
+# def  oldest( people1,  people2,  people3):
+#     if people1 > people2 > people3:
+#         print("poqr --", people3, 'mec ---', people1)
+#     elif people1 > people3 > people2:
+#         print("poqr --", people2, 'mec ---', people1)
+#     elif people2 > people1 > people3:
+#         print("poqr --", people3, 'mec ---', people2)
+#     elif people2 > people3 > people1:
+#         print("poqr --", people1, 'mec ---', people2)
+#     elif people3 > people2 > people1:
+#         print("poqr --", people1, 'mec ---', people3)
+#     elif people3 > people1 > people2:
+#         print("poqr --", people2, 'mec ---', people3)
+#     else:
+#         print("havasar")
+# oldest( 10, 60, 15)
+
+
+print("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
